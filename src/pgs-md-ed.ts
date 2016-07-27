9c6a7b70644f33203b7e65241a8b781810e0f0f0
tpluscode <tpluscode@users.noreply.github.com>
2016-07-27 20:39:34 +0200

    some tests

---
+++ src/pgs-md-ed.ts
@@ -0,0 +1,10 @@
+/// <reference path="../bower_components/polymer-ts/polymer-ts.d.ts" />
+
+@component('pgs-md-ed')
+class PgsMdEd extends polymer.Base {
+
+    @property()
+    markdown: String;
+}
+
+PgsMdEd.register();
