61bb18749fb925166de4cb8a96b35d0c80c66380
tpluscode <tpluscode@users.noreply.github.com>
2016-07-27 20:05:38 +0200

    init

---
+++ config.js
@@ -0,0 +1,26 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "html": "github:Hypercubed/systemjs-plugin-html@0.0.8",
+    "typescript": "npm:typescript@1.8.10",
+    "github:Hypercubed/systemjs-plugin-html@0.0.8": {
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.7.22"
+    },
+    "github:jspm/nodelibs-os@0.1.0": {
+      "os-browserify": "npm:os-browserify@0.1.2"
+    },
+    "npm:os-browserify@0.1.2": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    },
+    "npm:typescript@1.8.10": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    }
+  }
+});
